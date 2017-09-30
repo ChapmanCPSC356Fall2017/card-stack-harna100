53bdb28f469cf10f3ec9f09a5043f91bcab805ef
Paul Harnack <harna100@mail.chapman.edu>
2017-09-29 23:13:48 -0700

    Completes basic assignment, maybe will add animations?

--- app/src/main/java/edu/chapman/cpsc356/harna100/cardstack/CardView.java
+++ app/src/main/java/edu/chapman/cpsc356/harna100/cardstack/CardView.java
@@ -1,6 +1,9 @@
 package edu.chapman.cpsc356.harna100.cardstack;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
@@ -21,11 +24,22 @@ public class CardView extends FrameLayout {
 	private ImageView iv_suit_mid;
 	private ImageView iv_suit_bot;
 
+	public CardView(@NonNull Context context, @Nullable AttributeSet attrs) {
+		super(context, attrs);
+		setViews();
+		cardModel = new CardModel(Suit.Heart, "A");
+	}
+
 	public CardView(Context context, AttributeSet attrs, CardModel cardModel){
 		super(context, attrs);
 		this.cardModel = cardModel;
 		LayoutInflater.from(context).inflate(R.layout.fl_card, this, true);
 
+		setViews();
+		setCard();
+	}
+
+	private void setViews(){
 		this.tv_top = (TextView) findViewById(R.id.tv_top_numb);
 		this.tv_bot = (TextView) findViewById(R.id.tv_bot_numb);
 
@@ -34,4 +48,63 @@ public class CardView extends FrameLayout {
 		this.iv_suit_bot = (ImageView) findViewById(R.id.iv_bot_suit);
 
 	}
+
+	private void setCard(){
+		setSuits();
+		setValue();
+		setTextColor();
+	}
+
+	private void setSuits(){
+		int toUse = R.drawable.ic_heart;
+
+		switch(cardModel.getSuit()){
+			case Heart:
+				toUse = R.drawable.ic_heart;
+				break;
+			case Diamond:
+				toUse = R.drawable.ic_diamond;
+				break;
+			case Club:
+				toUse = R.drawable.ic_club;
+				break;
+			case Spade:
+				toUse = R.drawable.ic_spade;
+				break;
+		}
+
+		iv_suit_top.setImageResource(toUse);
+		iv_suit_mid.setImageResource(toUse);
+		iv_suit_bot.setImageResource(toUse);
+	}
+
+	private void setValue(){
+		tv_top.setText(cardModel.getValue());
+		tv_bot.setText(cardModel.getValue());
+	}
+
+	private void setTextColor(){
+		int toUse = Color.RED;
+
+		switch(cardModel.getSuit()){
+			case Heart:
+			case Diamond:
+				toUse = Color.RED;
+				break;
+			case Club:
+			case Spade:
+				toUse = Color.BLACK;
+				break;
+		}
+
+		tv_top.setTextColor(toUse);
+		tv_bot.setTextColor(toUse);
+	}
+
+
+	public void changeCardModel(CardModel newModel){
+		this.cardModel = newModel;
+		setCard();
+	}
+
 }
--- app/src/main/java/edu/chapman/cpsc356/harna100/cardstack/MainActivity.java
+++ app/src/main/java/edu/chapman/cpsc356/harna100/cardstack/MainActivity.java
@@ -1,13 +1,76 @@
 package edu.chapman.cpsc356.harna100.cardstack;
 
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
+
+	private static final String TAG = "MainActivity";
+	private FrameLayout fl_cards;
+	private Stack<CardModel> cardStack;
+	private CardView cardView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		fl_cards = (FrameLayout) findViewById(R.id.fl_cards);
+		cardView = new CardView(this, null, new CardModel(Suit.Club, "4"));
+		fl_cards.addView(cardView);
+		fillStack();
+		cardView.changeCardModel(cardStack.pop());
+	}
+
+	public void changeCards(View view) {
+		if(!cardStack.isEmpty()){
+			cardView.changeCardModel(cardStack.pop());
+		}
+		else{
+			finish();
+		}
+
+	}
+
+	private void fillStack(){
+		List<CardModel> cards = new ArrayList<CardModel>();
+		int count = 0;
+		for (Suit suit : Suit.values()) {
+			for (int i = 1; i < 14; ++i) {
+				String toUse;
+				switch (i) {
+					case 1:
+						toUse = "A";
+						break;
+					case 11:
+						toUse = "J";
+						break;
+					case 12:
+						toUse = "Q";
+						break;
+					case 13:
+						toUse = "K";
+						break;
+					default:
+						toUse = Integer.toString(i);
+						break;
+				}
+				cards.add(count++, new CardModel(suit, toUse));
+			}
+		}
+		Collections.shuffle(cards);
+		cardStack = new Stack<CardModel>();
+		for (CardModel card : cards) {
+			cardStack.push(card);
+		}
 	}
 }
